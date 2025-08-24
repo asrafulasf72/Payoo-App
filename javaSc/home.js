e7c050cdc0037c75cc783a7d92d476254fc20376
asraful <asrafulislamasf@gmail.com>
2025-08-24 22:24:18 +0600

    Withdrawal features have been added

--- javaSc/home.js
+++ javaSc/home.js
@@ -19,6 +19,41 @@ document.getElementById('add-money-btn')
     }
     const totalBalance=AvailableBalance+addAmount;
     document.getElementById('available-balance').innerText=totalBalance;
+})
 
+document.getElementById('add-money-toggle-btn')
+.addEventListener('click', function(){
+     document.getElementById('cash-out-toggle').style.display='none';
+    document.getElementById('add-money-toggle').style.display='block';
+})
+
+document.getElementById('cash-out-toggle-btn')
+.addEventListener('click', function(){
+    document.getElementById('cash-out-toggle').style.display='block';
+    document.getElementById('add-money-toggle').style.display='none';
+})
+
+document.getElementById('btn-witdraw')
+.addEventListener('click',function(e){
+    e.preventDefault();
+    
+   const agentNumber=document.getElementById('agent-Number').value;
+   const PinNumber= parseInt(document.getElementById('pin-Snumber').value);
+   const WitdrawBalance=parseInt(document.getElementById('withdraw-amount').value);
+   const AvailableBalance=parseInt(document.getElementById('available-balance').innerText);
    
+   const totalNewBalance=AvailableBalance-WitdrawBalance;
+
+   if(agentNumber.length<11){
+    alert('Enter Valid Agent Number')
+    return
+   }else if(PinNumber!=validPin){
+    alert('Enter Valid Pin')
+    return
+   }else if(AvailableBalance===0 || AvailableBalance<WitdrawBalance){
+    alert('Balance Low');
+    return
+   }
+   document.getElementById('available-balance').innerText=totalNewBalance;
+
 })
